b23fb61c4112cac16f2d494eb8c59d311a071a7c
pavankumar-ui <pavankumar.codedekho@gmail.com>
2024-11-22 20:46:31 +0530

    test cases is running good and passed,then changed the app code correctly

--- app.js
+++ app.js
@@ -8,16 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(taskRouter);
 
-app.listen(PORT, (err) => {
-    if (err) {
-        return console.log("Something bad happened", err);
-    }
-    console.log(`Server is listening on ${PORT}`);
-});
 
 //to catch all the exceptions in the middleware//
 app.use((req, res) => {
     res.status(404).json({ error: "Not found! invalid input" });
 });
+
+
+if(require.main === module) {
+    app.listen(PORT, (err) => {
+      if (err) {
+        return console.log("Something bad happened", err);
+      }
+      console.log(`Server is listening on ${PORT}`);
+    });
+    }
 
 module.exports = app;
